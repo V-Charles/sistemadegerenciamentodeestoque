bdbf529f0c2318617c52c7a4e626b89713abe8ec
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-05-06 12:59:02 -0300

    Projeto criado

---
+++ src/main/java/com/projetointegrador/sistemaestoque/SistemaestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.projetointegrador.sistemaestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaestoqueApplication.class, args);
+	}
+
+}
